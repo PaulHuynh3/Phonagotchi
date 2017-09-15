008c5ee97ecad1e62375786ba339be9cab94cc85
Paul <paul.huynh3@gmail.com>
2017-09-15 00:14:08 -0400

    add moving apple

--- Phonagotchi/LPGViewController.m
+++ Phonagotchi/LPGViewController.m
@@ -12,8 +12,10 @@
 
 @interface LPGViewController ()
 
-@property (nonatomic) UIImageView *petImageView;
-@property Pet *pet;
+@property (nonatomic, strong) UIImageView *petImageView;
+@property (nonatomic,strong) Pet *pet;
+@property (nonatomic, strong) UIImageView *apple;
+
 
 
 @end
@@ -55,9 +57,10 @@
                                  multiplier:1.0
                                    constant:0.0].active = YES;
     
-    //has to be on the bottom as the code loads my gesture will be next..
+    //has to be on the bottom as the code loads my gesture will be next.. only these 3 methods have to be added under didLoad b/c they contain the other methods in the selector()
     [self petGesture];
-    [self addBucket];
+    UIImageView *bucket =[self addBucket];
+    [self addAppleToBucket:bucket];
     
 }
 
@@ -88,7 +91,7 @@
 
     UIImageView *bucketImage = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"bucket"]];
     CGRect frame = CGRectMake(0, 0, 100, 100);
-    //origin is a built in CGPoint property to call with x and y
+    //origin lets us manipulate the x,y coordinates
     frame.origin.x = 20;
     frame.origin.y = CGRectGetMaxY(self.view.frame) - frame.size.height; // .size .view .heigh all built in CGPOINT
     bucketImage.frame = frame; //has to equal the new frames
@@ -97,7 +100,56 @@
 
 }
 
--(UIImage)
+-(UIImageView*)addAppleToBucket:(UIImageView*)bucket {
+    UIImageView *appleImage = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"apple"]];
+    CGRect frame = CGRectMake(0, 0, 50, 50);
+    //our apple's x value is relative to the bucket
+    frame.origin.x = 20 + bucket.frame.size.width/2;
+    frame.origin.y = CGRectGetMaxY(self.view.frame) - bucket.frame.size.height/2 - frame.size.height;
+    //make the two new origins equal to the new frame
+    appleImage.frame = frame;
+    
+    UIPinchGestureRecognizer *pinch =[[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(handlePinch:)];//uses method handlepinch
+    [appleImage addGestureRecognizer:pinch];
+    appleImage.userInteractionEnabled = YES;
+    [self.view addSubview:appleImage];
+    
+    return appleImage;
+}
+
+-(void)handlePinch:(UIPinchGestureRecognizer*)pinch {
+    UIImageView *apple = (UIImageView*)pinch.view;
+    UIImageView *otherApple = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"apple"]];
+    CGRect frame = apple.frame;
+    frame.origin.x = frame.origin.x + 5;
+    frame.origin.y = frame.origin.y - 5;
+    otherApple.frame = frame;
+    otherApple.userInteractionEnabled = true;
+    [self.view addSubview:otherApple];
+    self.apple = otherApple;
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(handleMoveApple:)]; //this has the method handlemove apple
+    [self.apple addGestureRecognizer:pan];
+}
+
+-(void)handleMoveApple:(UIPanGestureRecognizer*)pan {
+    CGRect frame = self.apple.frame;
+    frame.origin = [pan locationInView:self.view];
+    self.apple.frame = frame;
+    if (pan.state == UIGestureRecognizerStateEnded) {
+        CGRect frame = self.apple.frame;
+        if (frame.origin.y < CGRectGetMaxY(self.petImageView.frame)) {
+            [self.apple removeFromSuperview];
+        } else {
+            [UIView animateWithDuration:500 animations:^{
+                CGRect frame = self.apple.frame;
+                frame.origin.y = CGRectGetMaxY(self.view.frame) + 100;
+                self.apple.frame = frame;
+                [self.apple removeFromSuperview];
+            }];
+            
+        }
+    }
+}
 
 
 @end
